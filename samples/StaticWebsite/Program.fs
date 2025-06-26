3d131347e3920563fa644e5e544bf3b434198fc0
Pim Brouwers <pimnation@gmail.com>
2025-06-26 10:43:46 -0400

    html module text functions, static website sample project

---
+++ samples/StaticWebsite/Program.fs
@@ -0,0 +1,40 @@
+﻿open System
+open System.IO
+open Falco.Markup
+
+// Components
+let divider =
+    _hr [ _class_ "divider" ]
+
+// Template
+let master (title : string) (content : XmlNode list) =
+    _html [ _lang_ "en" ] [
+        _head [] [
+            _title [] [ _text "Sample App" ]
+        ]
+        _body [] content
+    ]
+
+// Views
+let homeView =
+    master "Homepage" [
+        _h1' "Homepage"
+        divider
+        _p' "Lorem ipsum dolor sit amet."
+    ]
+
+let aboutView =
+    master "About Us" [
+        _h1' "About"
+        divider
+        _p' "Lorem ipsum dolor sit amet."
+    ]
+
+// Generate website
+let writeHtmlToFile (filename : string) (html : XmlNode) =
+    File.WriteAllText(
+        Path.Join(__SOURCE_DIRECTORY__, filename),
+        renderHtml html)
+
+writeHtmlToFile "_homepage.html" homeView
+writeHtmlToFile "_about.html" aboutView
